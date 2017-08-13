f144e55524036cd20586c510ea9cb595a295cce9
XiaoLong Chen <xiaolong.chen01@sap.com>
2017-08-13 20:44:51 +0800

    fixed supply location service

--- supply-location-service/src/main/java/demo/SupplyLocationRepository.java
+++ supply-location-service/src/main/java/demo/SupplyLocationRepository.java
@@ -1,13 +1,13 @@
 package demo;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.geo.Point;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-import java.awt.*;
-import java.awt.print.Pageable;
 
 /**
  * Created on 8/13/2017.
@@ -19,5 +19,5 @@ public interface SupplyLocationRepository extends PagingAndSortingRepository<Sup
     SupplyLocation findFirstByLocationNear(@Param("location")Point location);
 
     @RestResource(path="cities")
-    Page<SupplyLocation> findByCity(@Param("city") String city, Pageable pageable);
+    Page<SupplyLocation> findByCity(@Param("city") String city, @Param("page") Pageable pageable);
 }
