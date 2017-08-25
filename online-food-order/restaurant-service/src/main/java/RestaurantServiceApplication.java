35f4d6a2495e0b2d9109f4b722342e2a1570c877
XiaoLong Chen <xiaolong.chen01@sap.com>
2017-08-26 00:51:40 +0800

    fixed the compile issue

---
+++ online-food-order/restaurant-service/src/main/java/RestaurantServiceApplication.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by XL on 8/26/2017.
+ */
+
+@SpringBootApplication
+public class RestaurantServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantServiceApplication.class, args);
+    }
+}
