dae812e7d22998f1bc34ffada3e22bef47ab7f3d
XiaoLong Chen <xiaolong.chen01@sap.com>
2017-08-29 23:57:18 +0800

    add message producer

--- online-food-order/order-service/src/main/java/com/ofo/service/OrderServiceImpl.java
+++ online-food-order/order-service/src/main/java/com/ofo/service/OrderServiceImpl.java
@@ -4,7 +4,11 @@ import com.ofo.domain.Order;
 import com.ofo.domain.Payment;
 import com.ofo.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.integration.support.MessageBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,11 +18,15 @@ import java.util.List;
  * Created by XL on 8/27/2017.
  */
 @Slf4j
+@EnableBinding(Source.class)
 @Service
 public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private MessageChannel output;
 
     private RestTemplate restTemplate = new RestTemplate();
 
@@ -29,10 +37,11 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public void pay(String orderId) {
-        String paymentService = "http://payment-service";
-        log.info("sending payment request to payment service");
+//        String paymentService = "http://payment-service";
+//        log.info("sending payment request to payment service");
         Order order = orderRepository.findOne(orderId);
         Payment payment = new Payment(order.getOrdedBy(), order.getPrice());
-        restTemplate.postForLocation(paymentService + "/api/paynow", payment);
+        //restTemplate.postForLocation(paymentService + "/api/paynow", payment);
+        this.output.send(MessageBuilder.withPayload(payment).build());
     }
 }
