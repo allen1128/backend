cfc3152a9b02833bd2c151a6a2fb7d68f2eaf94c
XiaoLong Chen <xiaolong.chen01@sap.com>
2017-09-01 01:05:34 +0800

    for rabbitmq debugging

---
+++ online-food-order/Rabbit-Test/src/main/java/com/xl/domain/CurrentPosition.java
@@ -0,0 +1,16 @@
+package com.xl.domain;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CurrentPosition {
+    private String runningId;
+    private Double speed;
+}
