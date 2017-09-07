e1d7efc1fd53618aa8e5e2498e126ae80971043f
Allen Chen <as_allen1128@hotmail.com>
2017-09-08 02:10:40 +0800

    added hystrix dependency

--- online-food-order/restaurant-service/src/main/java/com/ofo/rest/RestaurantRestController.java
+++ online-food-order/restaurant-service/src/main/java/com/ofo/rest/RestaurantRestController.java
@@ -1,73 +1,79 @@
 package com.ofo.rest;
 
-import com.ofo.domain.Address;
 import com.ofo.domain.CreditCard;
-import com.ofo.domain.Dish;
 import com.ofo.domain.Restaurant;
 import com.ofo.service.RestaurantService;
-import com.ofo.service.RestaurantServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by XL on 8/26/2017.
  */
 
-@RequestMapping(value="/restaurants")
+@RequestMapping(value = "/restaurants")
 @RestController
+@Slf4j
 public class RestaurantRestController {
 
     @Autowired
     RestaurantService restaurantService;
 
-    @RequestMapping(value="/search/{restaurantName}", method= RequestMethod.POST)
-    public List<Restaurant> findByRestaurantName(@PathVariable String restaurantName){
+    @RequestMapping(value = "/search/{restaurantName}", method = RequestMethod.POST)
+    public List<Restaurant> findByRestaurantName(@PathVariable String restaurantName) {
         return restaurantService.findByName(restaurantName);
     }
 
-    @RequestMapping(value="/", method = RequestMethod.POST)
+    @RequestMapping(value = "/", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public void upload(@RequestBody List<Restaurant> restaurants){
+    public void upload(@RequestBody List<Restaurant> restaurants) {
         restaurantService.saveRestaurantInfo(restaurants);
     }
 
-    @RequestMapping(value="/", method = RequestMethod.GET)
-    public List<Restaurant> findAll(){
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<Restaurant> findAll() {
         return restaurantService.findAll();
     }
 
-    @RequestMapping(value="/{restaurantId}", method=RequestMethod.DELETE)
-    public void delete(@PathVariable Long restaurantId){
+    @RequestMapping(value = "/{restaurantId}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long restaurantId) {
         restaurantService.deleteByRestaurantId(restaurantId);
     }
 
-    @RequestMapping(value="/{restaurantId}", method=RequestMethod.GET)
-    public Restaurant findOne(@PathVariable Long restaurantId){
+    @RequestMapping(value = "/{restaurantId}", method = RequestMethod.GET)
+    public Restaurant findOne(@PathVariable Long restaurantId) {
         return restaurantService.findOne(restaurantId);
     }
 
-    @RequestMapping(value="/addtocart/{dishId}/{quantity}", method=RequestMethod.POST)
-    public Long addToCart(@PathVariable Long dishId, @PathVariable int quantity){
+    @RequestMapping(value = "/addtocart/{dishId}/{quantity}", method = RequestMethod.POST)
+    public Long addToCart(@PathVariable Long dishId, @PathVariable int quantity) {
         return restaurantService.addToCart(dishId, quantity);
     }
 
-    @RequestMapping(value="/removefromcart/{dishId}", method=RequestMethod.POST)
-    public Long remoteFromCart(@PathVariable Long dishId){
+    @RequestMapping(value = "/removefromcart/{dishId}", method = RequestMethod.POST)
+    public Long remoteFromCart(@PathVariable Long dishId) {
         return restaurantService.removeFromCart(dishId);
     }
 
-    @RequestMapping(value="/addnotetocart", method=RequestMethod.POST)
-    public Long addNote(@RequestBody String note){
+    @RequestMapping(value = "/addnotetocart", method = RequestMethod.POST)
+    public Long addNote(@RequestBody String note) {
         return restaurantService.addNoteToCart(note);
     }
 
-    @RequestMapping(value="pay/{cartId}", method = RequestMethod.POST)
-    public boolean pay(@PathVariable Long cartId, @RequestBody CreditCard creditCard){
+    @RequestMapping(value = "pay/{cartId}", method = RequestMethod.POST)
+    public boolean pay(@PathVariable Long cartId, @RequestBody CreditCard creditCard) {
         return restaurantService.pay(cartId, creditCard);
+    }
+
+    @RequestMapping(value = "/username/{userName}", method = RequestMethod.POST)
+    public void setUserName(@PathVariable String userName, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("userName", userName);
+        log.info("username: " + session.getAttribute("userName"));
     }
 }
